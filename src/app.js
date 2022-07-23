e5a2d0782f26aab55800ff04bc99ba95050653bd
Neetu-12 <neetus21@navgurukul.org>
2022-07-23 12:16:11 +0530

    refactor

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const {connect,disconnect}=require("./database/databaseConfig")
 connect()   
 
 const app=express()
-const port=process.env.PORT || 27012
+const port=process.env.PORT || 8000
 app.set("view engine","hbs")
 
 let tempelate_path=path.join(__dirname,"../public/template")
